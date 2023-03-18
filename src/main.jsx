b3632d03a8ae8b9639811a2a2b71fed030dff9db
LucasNF22 <lucasnfiorentino@gmail.com>
2023-03-18 11:46:16 -0300

    Update en <MultipleCustomHooks />

--- src/main.jsx
+++ src/main.jsx
@@ -11,7 +11,7 @@ import "./index.css";
 // import { FormWithCustomHook } from './02-useEfect/FormWithCustomHook'
 // import { SimpleForm } from './02-useEfect/SimpleForm'
 // import { HookApp } from './HookApp'
-// import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks'
+import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks'
 // import { FocusScreen } from './04-useRef/FocusScreen'
 // import { Layout } from './05-useLayoutEffect/Layout'
 // import { Memorize } from './06-memos/Memorize'
@@ -27,6 +27,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
         {/* <React.StrictMode> */}
             <>
                 <MainApp /> 
+                <MultipleCustomHooks /> 
             </>
         {/* </React.StrictMode>,  */}
     </BrowserRouter>
--- test/03-examples/MultipleCustomHooks.test.js
+++ test/03-examples/MultipleCustomHooks.test.js
@@ -1,16 +1,101 @@
-import { getByAltText, render, screen } from '@testing-library/react'
+import { fireEvent, render, screen } from "@testing-library/react";
+import { MultipleCustomHooks } from "../../src/03-examples/MultipleCustomHooks";
+import { useCounter } from "../../src/hooks/useCounter";
+import { useFetch } from "../../src/hooks/useFetch";
 
-import { MultipleCustomHooks } from '../../src/03-examples/MultipleCustomHooks'
+jest.mock("../../src/hooks/useFetch");
+jest.mock("../../src/hooks/useCounter");
 
-describe('Prueba en el < MultipleCustomHook /> ', () => {
 
-    test('de mostrar el componente por defecto', () => {
+describe("Prueba en el < MultipleCustomHook /> ", () => {
 
-      render( <MultipleCustomHooks /> );
-      screen.debug();
-      expect( getByText( 'Buscando tu Pokemon...' ) )
+    const mockIncrement = jest.fn();
 
+    useCounter.mockReturnValue({
+        counter: 1,
+        increment: mockIncrement,
+    });
+
+    beforeEach( () => {
+        jest.clearAllMocks();
+    });
+    
+    test("de mostrar el componente por defecto", () => {
+        
+        useFetch.mockReturnValue({
+            data: null,
+            isLoading: true,
+            hasError: null
+        });
+
+        render(<MultipleCustomHooks />);
+        // screen.debug();
+        expect(screen.getByText("Buscando tu Pokemon..."));
+        expect(screen.getByText("Pokemon - Api"));
+
+        const nextButton = screen.getByRole("button", { name: "Siguiente" });
+        expect(nextButton.disabled).toBeTruthy;
+    });
+
+    test("Debe mostrar una Pokemón", () => {
+        
+       
+        useFetch.mockReturnValue({
+            data : {
+                id: 123, 
+                name: 'Pikachu', 
+                types: [
+                        {                        
+                        slot:1,
+                        type: {
+                                name:'Electrico',
+                                url:'www.pokemon.com/pikachu.png'
+                              }
+                        }],
+                },
+            isLoading: false,
+            hasError: null,
+        });
+        
+        
+        render(<MultipleCustomHooks />);
+        
+        const nextButton = screen.getByRole("button", { name: "Siguiente" });
+        const imgUrl = screen.getByRole("img", { name: "Card image cap" });
+
+        expect( screen.getByText('Pikachu') ).toBeTruthy;
+        expect( screen.getByText('Electrico') ).toBeTruthy;     
+        expect( nextButton.disabled ).toBeFalsy();
+        expect( imgUrl.src ).toBe("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/123.png");
+    });
+
+    test('Debe llamar a la funcion de incrementar', () => {
+        useFetch.mockReturnValue({
+            data : {
+                id: 123, 
+                name: 'Pikachu', 
+                types: [
+                        {                        
+                        slot:1,
+                        type: {
+                                name:'Electrico',
+                                url:'www.pokemon.com/pikachu.png'
+                              }
+                        }],
+                },
+            isLoading: false,
+            hasError: null,
+            
+        });
+
+        
+        render(<MultipleCustomHooks />);
+        const nextButton = screen.getByRole("button", { name: "Siguiente" });
+        fireEvent.click( nextButton );
+
+        expect( mockIncrement ).toHaveBeenCalled();
 
     })
 
-})
+//
+});
