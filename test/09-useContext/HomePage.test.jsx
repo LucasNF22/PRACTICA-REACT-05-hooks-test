f7150d02db384f508a78ac07c6d2b0ccde27bd3f
LucasNF22 <lucasnfiorenitno@gmail.com>
2023-03-20 13:59:08 -0300

    Inicio de test en UseContext <HomePage/>

---
+++ test/09-useContext/HomePage.test.jsx
@@ -0,0 +1,40 @@
+import { render, screen } from "@testing-library/react";
+import { UserContext } from "../../src/09-useContext/context/UserContext";
+import { UserProvider } from "../../src/09-useContext/context/UserProvider";
+import { HomePage } from "../../src/09-useContext/HomePage";
+
+describe("Pruebas en el componente de HomePage", () => {
+
+  const user = {
+    id:1,
+    name:'Lucas',
+  }
+ 
+  test("Debe mostrar el componente sin el usuario", () => {
+    render(
+      <UserContext.Provider value={{ user: null }}>
+        <HomePage />
+      </UserContext.Provider>
+    );
+
+    // screen.debug();
+    const preTag = screen.getByLabelText("pre");
+    // console.log(preTag.innerHTML);
+    expect( preTag.innerHTML ).toBe( 'null' );
+        
+  });
+  test("Debe mostrar el componente con el usuario", () => {
+    render(
+      <UserContext.Provider value={{ user }}>
+        <HomePage />
+      </UserContext.Provider>
+    );
+
+    screen.debug()
+    
+    const preTag = screen.getByLabelText("pre");
+    expect( preTag.innerHTML ).toContain( user.name );
+    expect( preTag.innerHTML ).toContain( `${user.id}` );
+        
+  });
+});
